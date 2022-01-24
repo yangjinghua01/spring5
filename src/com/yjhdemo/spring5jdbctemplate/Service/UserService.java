3b329d821043d70563065442a37423fa272f9c7e
16920 <1692047690@qq.com>
2022-01-24 23:02:16 +0800

    对于事务的基本认知和一个小小的demo 2022年1月24日 23:01:53 对事务的初步理解

--- src/com/yjhdemo/spring5jdbctemplate/Service/UserService.java
+++ src/com/yjhdemo/spring5jdbctemplate/Service/UserService.java
@@ -3,17 +3,35 @@ package com.yjhdemo.spring5jdbctemplate.Service;
 import com.yjhdemo.spring5jdbctemplate.Dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class UserService {
     @Autowired
     private UserDao userDao;
 
     //    转账的方法
     public void accountMoney() {
+//        try {
+////        开启事务操作
+////第二步进行业务操作
+////                lucy少100
+//            userDao.reduceMoney();
+////        mary多100
+//            int i = 10 / 0;
+//            userDao.addMoney();
+////            第三步如果没出现异常提交事务
+//        } catch (Exception e) {
+////出现异常回滚
+//
+//        }
+
+
 //    lucy少100
         userDao.reduceMoney();
 //        mary多100
+        int i =10/0;
         userDao.addMoney();
     }
 }
